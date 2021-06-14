146ab90f1d13d98918ac4c62f24630e8e583c493
miguelmba2000 <83592810+miguelmba2000@users.noreply.github.com>
2021-06-14 13:43:08 +0100

    Add files via upload

---
+++ CV3.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>cv/developpeur web</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="CSS/CV3.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-compatible" content="ie=edge chrome=1">
+	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
+	<link rel="stylesheet" type="text/css" href="CSS/animate.css">
+	<script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
+	<script type="text/javascript" src="js/CV2.js"></script>
+	<script type="text/javascript" src="js/wow.min.js"></script>
+	<script>new WOW().init();</script>	
+	<script type="text/javascript">
+		/*window.AddEventListener("scroll",function(){
+	var header=document.queryselector("header");
+	header.main.toggle("liens",window.scrolly>0);
+});*/
+
+	</script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+$('.menu-icone').click(function(){
+$('.container-menu').show();
+$('.main').show();	
+$('body').css({
+
+	overflow:'hidden',
+})
+});
+$('.hide-menu').click(function(){
+$('.container-menu').hide();
+$('.main').hide();
+$('body').css({
+
+	overflow:'scroll',
+})	
+
+});
+$('.container-menu ul li').click(function(){
+$('.container-menu').hide();
+$('.main').hide();
+$('body').css({
+
+	overflow:'scroll',
+})	
+
+});
+
+	
+});
+	</script>		
+</head>
+<body onload="animation()">
+<div class="general">
+	<header>
+<div class="container-global block0" id="block0">
+<div class="container-menu">
+<span style="float: right;" class="hide-menu">	Fermer</span>	
+	<ul class="main" >
+			<li><a href="#block6"> Contact</a></li><br>	
+			<li><a href="#block5"> Education et Expérience</a></li><br>	
+			<li><a href="#block4">Compétences </a></li><br>	
+			<li><a href="#block3">Formations </a></li><br>	
+			<li><a href="#block2"> A propos</a></li><br>	
+		</ul>
+</div>	
+	<div class="container1a">
+		<h3 class="mba">MMMR</h3>
+	</div>
+	<div class="container1b">
+		<div class="liens ">
+		<ul class="main" >
+			<li><a href="#block6"> Contact</a></li>
+			<li><a href="#block5"> Education et Expérience</a></li>
+			<li><a href="#block4">Compétences </a></li>
+			<li><a href="#block3">Formations </a></li>
+			<li><a href="#block2"> A propos</a></li>
+		</ul>
+			</div>
+	</div>
+		<div class="menu-icone">
+<i class="fa fa-bars" ></i>
+
+</div>
+</div>
+</header>
+<div class="container-global" id="block1">
+	<div class="container2a">
+		<div class="box1a">
+				<p>Salut, je suis</p>
+			</div>
+			<h1 class="nom"><b>MBA MBA MIGUEL RODRIGUE</b></h1> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="nom2">Web developpeur/Designer</span><br>
+			<div class="nom3">
+		<i class="far fa-envelope fa-1x"></i>&nbsp;&nbsp;<a href="mailto:mbamiguel2000@gmail.com">mbamiguel2000@gmail.com</a><br/><i class="fas fa-phone-square fa-1x"></i>&nbsp;&nbsp; +237 659 158 386<br/> <i class="fa fa-map-marker-alt fa-1x"></i>&nbsp;&nbsp; Cameroun, yaounde 
+		</div>
+		<div class="font">
+<i class="fab fa-facebook-square"></i>
+<i class="fab fa-twitter"></i>
+<i class="fab fa-instagram"></i>
+<i class="fab fa-google"></i> 
+</div>
+	</div>
+	<div class="container2b">
+					<img src="mba1.jpg">
+	</div>
+</div>
+<div class="container-global" id="block2">
+	<div class="container3a">
+	<img src="mbc3.jpg">
+	</div>
+	<div class="container3b">
+		<h2><u>A propos de Moi</u></h2>
+	<mark>2012:</mark> J'ai obtenu mon CEPE au Cebec de Mballa V.<br/><br/>
+	 <mark>2016:</mark> J'ai été diplomé d'un BEPC au LBNE de Yaounde.<br/><br/>
+	<mark>2018:</mark> Obtenu mon Probatoire (D) au LBNE de Yaounde.<br/><br/>
+	<mark>2019:</mark> J'ai eu mon baccalauréat à l'Institut  MATAMFEN.<br/><br/>
+	<mark>2021:</mark> BTS en génie Logiciel à ISTAG de Yaoundé.<br/>
+	<button class="line"><a href="cv online.pdf" download="Online CV"> Download cv&nbsp;&nbsp;<i class="fas fa-download"></i></button></a>
+	</div>
+</div>
+<div class="container-global" id="block3">
+			<h2 class="for"><u>Formation</u></h2>
+	<div class="container4a">
+				<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="far fa-clone fa-2x"></i></p>
+				<h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Web Designer</h2>
+				<p class="p1">J'ai fait ma formation web designer en deux ans et je suis sortis major de ma formation. J'ai fais les stage dans des très grandes entreprises en tant que web designer notamment SEINOVA.SAL qui est au CAMEROUN plus precisement a Total Caveau.</p>
+	</div>
+	<div class="container4b">
+		<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fas fa-tape fa-2x"></i></p>
+				<h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;App developpeur</h2>
+				<p class="p1">J'ai fais ma formation après mes deux ans de formations. j'ai développé en premiere année une application qui gère les patients d'une clinique en Visual Basic avec un collegue. Je suis passionné par le développement des applications web.</p> 
+	</div>
+	<div class="container4c">
+		<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i style="color: #1d8bda" class="far fa-object-ungroup fa-2x "></i></p>
+				<h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Web développeur</h2>
+				<p class="p1">J'ai fais ma formation en développement d'application au même moment que ma formation en développement web. J'ai developpé des sites web qui font dans la vente des appareils électroniques et des téléphones.</p>
+	</div>
+</div>
+<div class="container-global" id="block4">
+	<div class="container5a">
+		<h2><u>Langages</u></h2><br/>
+				<div class="niv1">HTML <span class="right">80%</span> 
+				<div class="rect wow slideInLeft" data-wow-duration="2s">
+					<div class="rect1 wow slideInLeft" data-wow-duration="4s"></div>
+			</div><br/>
+				CSS  <span  class="right">80%</span> 
+				<div class="car wow slideInLeft" data-wow-duration="2s">
+					<div class="car1 wow slideInLeft" data-wow-duration="4s"></div>
+				</div><br/> 
+				PHP <span  class="right">50%</span> 
+				<div class="tri wow slideInLeft" data-wow-duration="2s">
+					<div class="tri1 wow slideInLeft" data-wow-duration="4s"></div>
+				</div><br/>
+				JQUERY  <span  class="right">50%</span> 
+				<div class="dern wow slideInLeft" data-wow-duration="2s">
+					<div class="dern1 wow slideInLeft" data-wow-duration="4s"></div>
+					</div><br>
+					VISUAL BASIC  <span  class="right">60%</span> 
+				<div class="dern wow slideInLeft" data-wow-duration="2s">
+					<div class="dern1 wow slideInLeft" data-wow-duration="4s" id="vb"></div>
+					</div>
+				</div>
+	</div>
+	<div class="container5b">
+		<h2><u>Langues</u></h2>
+				<div class="rond1 wow bounceInRight" data-wow-duration="3s"><p class="fr">80%<p> 
+					<p class="lang1">Français</p>
+				</div>
+				<div class="rond2 wow bounceInRight" data-wow-duration="4s"><p class="ang">50%</p>
+				 <p class="lang1">Anglais</p>
+				</div>
+	</div>
+	</div>
+	<div class="container-global" id="block5">
+		<div class="container-global1">
+		<h2 class="pe"><u>Education</u></h2>
+		<div class="container6a">
+		<p>J'ai fais mon école maternelle et primaire au CEBEC DE MBALLA V qui est  situé sis à Manguier. La culture que j'ai apprise de là était une meilleure éducation.</p>
+	</div>
+	<div class="container6b">
+	<p>Arrivé au Sécondaire, j'ai été entretenue par un professseur de français qui m'éduquais comme si j'étais l'un de ses leurs. Et elle ausi a participé à mon éducation scolaire.</p>	
+	</div>
+	<div class="container6c">
+		
+	</div>
+	</div>
+	<div class="container-global1">
+	<h2 class="pe"><u>Expérience</u></h2>
+		<div class="container6a">
+		<P> Ma prémière expérience a été acquise dans une entreprise Camerounaise situé à Yaounde d'où j'ai realisé mon tout prémier cv pour le mettre en ligne </P>
+	</div>
+	<div class="container6b">
+		<p>Ma deuxième a été réalisé toujours dans la même entreprise camerounaise (Seinova Sarl) avec qui j'ai augmenté mes acquis  </p>
+	</div>
+	<div class="container6c">
+		
+	</div>
+	</div>
+	</div>
+	<div class="container-global" id="block-image">
+		<h2 class="titre"><u>portfolio</u></h2>
+		<div class="container7a">
+			<div class="box7a1">
+			<img class="image" id="image1" src="url2.jpg">
+			
+			</div>
+			<div class="box7a1">
+		<img class="image" src="ima1.webp">
+				
+			</div>
+			<div class="box7a1">
+		<img  class="image"src="ima2.jpg">
+				
+			</div>
+		</div>
+		<div class="container7a">
+			<div class="box7a1">
+			<img class="image" src="ima3.jpg">
+			
+			</div>
+			<div class="box7a2">
+		<img  class="image"src="ima4.jpg">
+				
+			</div>
+			<div class="box7a3">
+		<img  class="image"src="ima5.jpg">
+				
+			</div>
+		</div>
+		<div class="container7a">
+			<div class="box7a1">
+		<img  class="image"src="ima6.jpg">
+				
+			</div>
+			<div class="box7a2">
+			<img  class="image"src="ima7.webp">
+			
+			</div>
+			<div class="box7a3">
+			<img  class="image" src="ima8.webp">
+	
+			</div>
+		</div>
+	</div>
+	<!--<div class="container-global">
+		<div class="container8">
+	<div class="container8a">
+		
+	</div>
+	<div class="container8b">
+		
+	</div>
+	<div class="container8c">
+		
+	</div>
+	</div>
+	</div>-->
+<div class="container-global" id="block6">
+				<h2 class="titre"><u>Contactez-moi</u></h2>
+
+		<div class="container9">
+	<div class="container9a">
+		<div class="box9a">
+			<i class="fab fa-telegram-plane fa-2x" ></i>
+					
+		</div>
+		<div class="box9b">
+			<p><b>Adresse: Yaoundé (Bp:5111)</b></p>
+					
+		</div>
+	</div>
+	<div class="container9b">
+		<div class="box9a">
+			<i class="far fa-envelope fa-2x"></i>
+		</div>
+		<div class="box9b">
+			<p><a href="mailto:mbamiguel2000@gmail.com"><b>mbamiguel2000@gmail.com</b></a></p>
+		</div>
+	</div>
+	<div class="container9c">
+		<div class="box9a">
+		<i class="fas fa-phone-square fa-2x"></i>	
+		</div>
+		<div class="box9b">
+			<p><b>Téléphone: +237 659 158 386</b></p>
+		</div>
+	</div>
+	</div>
+	</div>
+	<div class="container10">
+		<div class="box10a">
+			<form id="message" method="POST" action="CV3.php" name="vform">
+			<span id="error">Veuillez remplir tous les champs</span><br/><br/>
+			<input type="text" placeholder="Entrez votre nom" name="nom" id="nom" >
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="prenom" placeholder="Entrez votre prenom" id="prenom"  ><span id="error2" name="val_prenom"></span><br/><br/>
+			<input type="email" name="mail" class="mail" placeholder="Entrez votre E-mail" id="mail" ><span id="error3" class="val_email"></span><br/><br/>
+			<textarea  id="text" name="text" placeholder="Votre message" ></textarea><br/><br/>
+
+			<input type="submit" class="submit" name="valider" value="Envoyer votre message" onclick="valider">
+		</form>
+		</div>
+		<div class="box10b">
+<i class="fab fa-facebook-square"></i>
+<i class="fab fa-twitter"></i>
+<i class="fab fa-instagram"></i>
+<i class="fab fa-google"></i>
+
+		</div>
+</div>
+<div class="footer">
+		<u>copyright by petit jesus 2020. Une utilisation sans mon avis est interdite !!!!!</u>
+
+</div>
+<?php  
+
+$connexion = new PDO("mysql:host=localhost;dbname=messagerie",'root','');
+
+if (isset($_POST['submit'])){
+	
+$nom=$_POST['nom'];
+$prenom=$_POST['prenom'];
+$mail=$_POST['mail'];
+$message=$_POST['text'];
+
+
+$req="INSERT INTO `Messagerie` VALUES (NULL, '$nom', '$prenom', '$mail', '$message')";
+
+
+$result = $connexion->exec($req);
+
+if ($result==true){
+	echo "bon";
+}else{
+	echo "mauv";
+}
+
+}
+ ?> 
+</div>
+<div id="loader">
+	
+</div>
+
+
+
+<script>
+var myVar;
+
+function animation() {
+  myVar = setTimeout( show,3000);
+}
+
+function show(){
+	$('#loader').hide();
+	$('.general').show();
+	
+
+}
+
+
+
+</script>
+</body>
+
+</html>
